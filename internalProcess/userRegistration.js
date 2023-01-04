c2b5b30f952afda7948f3560a9f9c404b27133cc
Lukáš Kníže <82168954+LukasKnize@users.noreply.github.com>
2023-01-04 12:20:54 +0100

    salt generation changes

--- internalProcess/userRegistration.js
+++ internalProcess/userRegistration.js
@@ -43,14 +43,7 @@ let signup = async (data) => {
 
 
 async function hashPassword(pass) {
-    let saltCharSet =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let salt = "";
-    for (let i = 0; i < 32; i++) {
-        salt += saltCharSet.charAt(
-            Math.floor(Math.random() * saltCharSet.length)
-        );
-    }
+    let salt = require('crypto').randomBytes(32).toString('hex')
 
     let peperArray = [
         "A",
